283ad59df9595229c5462c1bef078c23476c1587
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2025-04-03 11:10:39 -0600

    feat(js): Added main js with logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 let state = 0;
 let soundBlender = new Audio("sounds/blender_on.mp3");
 let soundButton = new Audio("sounds/button.mp3");
+let blender = document.getElementById("blender");
+let button = document.getElementById("blender_button");
+
+let green = "#00FF0044";
+let red = "#FF000044";
 
 function switchBlender() {
     if (state === 0) {
+        activateBlender();
         state = 1;
     }
     else {
+        deactivateBlender();
         state = 0;
     }
     playSounds();
@@ -23,4 +30,14 @@ function playSounds() {
         soundBlender.pause();
         soundBlender.currentTime = 0;
     }
+}
+
+function activateBlender() {
+    blender.classList.add("active");
+    button.style.backgroundColor = red;
+}
+
+function deactivateBlender() {
+    blender.classList.remove("active");
+    button.style.backgroundColor = green;
 }
